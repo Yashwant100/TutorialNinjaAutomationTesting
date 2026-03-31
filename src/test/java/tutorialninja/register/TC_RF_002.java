ae8f44adf4656f07688fd583b716a52275d6a932
Yashwant <yashii9885@gmail.com>
2026-03-31 14:25:21 +0530

    TC_RF_002

--- src/test/java/tutorialninja/register/TC_RF_002.java
+++ src/test/java/tutorialninja/register/TC_RF_002.java
@@ -9,44 +9,48 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TC_RF_001 {
-    @Test
-	public void verifyRegisteringWithMandatoryFields() {
+public class TC_RF_002 {
+
+	@Test
+	public static void verifyRegisterAccountWithAllFields() {
 		// LAunching Browser
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
+
 		// Opening URL
 		driver.get("https://tutorialsninja.com/demo/");
 		driver.manage().window().maximize();
-		
+
 		// LOcating element
 		driver.findElement(By.xpath("//span[text()='My Account']")).click();
 		driver.findElement(By.linkText("Register")).click();
-		
+
 		driver.findElement(By.id("input-firstname")).sendKeys("Yashwant");
 		driver.findElement(By.id("input-lastname")).sendKeys("Saini");
-		driver.findElement(By.id("input-email")).sendKeys(generateEmailWithTimestamp());
+		driver.findElement(By.id("input-email")).sendKeys(TC_RF_001.generateEmailWithTimestamp());
 		driver.findElement(By.id("input-telephone")).sendKeys("123456");
 		driver.findElement(By.id("input-password")).sendKeys("Yashwant123");
 		driver.findElement(By.id("input-confirm")).sendKeys("Yashwant123");
+		driver.findElement(By.xpath("//input[@name='newsletter'][@value='1']"));
 		driver.findElement(By.name("agree")).click();
 		driver.findElement(By.xpath("//input[@value='Continue']")).click();
-		
+
 		// Validating if the account is created or not
 		Assert.assertTrue(driver.findElement(By.linkText("Logout")).isDisplayed());
-		
+
 		String expectedHeading = "Your Account Has Been Created!";
-		Assert.assertEquals(driver.findElement(By.xpath("//div[@id='common-success']//h1")).getText(),expectedHeading);
-		
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@id='common-success']//h1")).getText(), expectedHeading);
+
 		driver.findElement(By.xpath("//a[text()='Continue']")).click();
 		Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
-	
-
+		
+		Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
+		
+		// Exit from the browser
+		driver.quit();
+		
 	}
 	
-	public static String generateEmailWithTimestamp() {
-		return new Date().toString().replaceAll("\\s","").replaceAll("\\:","")+"@gmail.com";
-	}
+	
 
 }
